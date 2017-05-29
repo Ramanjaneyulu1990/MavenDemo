bb8a512b0c2d482e77fd62055730abe239c3671d
BRamanjaneyulu <ramaws1990@gmail.com>
2017-05-29 14:46:01 +0530

    jenkinsfile sample

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld{
 
     public static void main(String[] args) {
         System.out.println("Welcome to world of Maven");
+        System.out.println("Welcome to world of jenins");
     }
 }
